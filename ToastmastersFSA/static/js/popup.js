05a741c79eaf0895bfd5151a2cf3f0885be41c00
Petiton Wiseley Paul-Enzer <wppet@ulaval.ca>
2025-12-29 23:33:52 -0500

    Update styles for logo and popup; enhance sidebar layout

--- ToastmastersFSA/static/js/popup.js
+++ ToastmastersFSA/static/js/popup.js
@@ -2,6 +2,20 @@ function openPopup(htmlContent) {
   document.getElementById("popup-body").innerHTML = htmlContent;
   document.getElementById("popup").style.display = "flex";
 }
+
 function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const popupOverlay = document.querySelector('.popup-overlay');
+  
+  if (popupOverlay) {
+    popupOverlay.addEventListener('click', (e) => {
+      
+      if (window.innerWidth <= 767 && e.target === popupOverlay) {
+        closePopup();
+      }
+    });
+  }
+});
