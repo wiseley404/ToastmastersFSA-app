b69203003306a3b7f85d06d8bda6baa7df567946
Petiton Wiseley Paul-Enzer <enzerpaul@gmail.com>
2025-12-30 01:16:39 -0500

    Adjust popup positioning and height for improved mobile experience

--- ToastmastersFSA/static/js/popup.js
+++ ToastmastersFSA/static/js/popup.js
@@ -8,45 +8,14 @@ function closePopup() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const popupOverlay = document.querySelector('.popup-overlay');
-    
-    if (popupOverlay) {
-        popupOverlay.addEventListener('click', (e) => {
-        
-        if (window.innerWidth <= 767 && e.target === popupOverlay) {
-            closePopup();
-        }
-        });
-    }
-
-    let startY = 0;
-    let currentY = 0;
-    
-    const popupContent = document.querySelector('.popup-content');
-    
-    popupContent?.addEventListener('touchstart', (e) => {
-    startY = e.touches[0].clientY;
-    popupContent.style.transition = 'none'; 
+  const popupOverlay = document.querySelector('.popup-overlay');
+  
+  if (popupOverlay) {
+    popupOverlay.addEventListener('click', (e) => {
+      
+      if (window.innerWidth <= 767 && e.target === popupOverlay) {
+        closePopup();
+      }
     });
-
-    popupContent?.addEventListener('touchmove', (e) => {
-    currentY = e.touches[0].clientY;
-    const deltaY = currentY - startY;
-    
-    if (deltaY > 0) {
-        popupContent.style.transform = `translateY(${deltaY})`;
-    }
-    });
-
-    popupContent?.addEventListener('touchend', (e) => {
-    const deltaY = currentY - startY;
-    
-    if (deltaY > 100 && window.innerWidth <= 767) {
-        closePopup(); 
-    } else {
-        popupContent.style.transition = 'transform 0.3s ease';
-        popupContent.style.transform = 'translateY(0)';
-    }
-    });
-
+  }
 });
